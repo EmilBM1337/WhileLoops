f2d986f3f420e6866100c6739e1535be1f08f111
emil41m5 <emil41m5@CV-BB-5340>
2021-08-18 11:51:50 +0200

    Add project files.

---
+++ Opgave 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Opgave_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 200;
+
+            int i = 0;
+            int a = 0;
+
+            Console.WriteLine("Jeg kan lægge 5 tal sammen.");
+            Console.WriteLine("");
+
+            while (i < 5)
+            {
+                Console.Write("Indtast et tal: ");
+                a += int.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Summen er: " + a);
+        }
+    }
+}
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 200;
+
+            int i = 0;
+
+            /*
+            while (i < 10)
+            {
+                Console.WriteLine($"While statement {i}");
+                i++;
+            }
+            */
+            
+            i = 10;
+            while (i > 0)
+            {
+                Console.WriteLine($"While statement {i}");
+                i--;
+            }
+            Console.WriteLine("Denne løkke har kørt 10 gange");
+        }
+    }
+}
