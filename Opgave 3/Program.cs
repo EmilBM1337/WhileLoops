5281e9d4039a6957c3dfb87db485c41a2bb4ff93
emil41m5 <emil41m5@CV-BB-5340>
2021-08-18 13:53:55 +0200

    .

---
+++ Opgave 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opgave_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv hvad du vil.... ");
+            string input = Console.ReadLine();
+            Console.WriteLine("");
+            int x = 0;
+
+            while (x < input.Length)
+            {
+                Console.WriteLine(input.Substring(x, 1));
+                x++;
+            }
+        }
+    }
+}
