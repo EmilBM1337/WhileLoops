9816fe19ca15ce5a2ae5c41589ee5a27d31ca956
emil41m5 <emil41m5@CV-BB-5340>
2021-08-18 14:36:01 +0200

    .

---
+++ Opgave 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Opgave_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            while (input != "exit")
+            {
+                input = Console.ReadLine().ToLower();
+                if (input == "exit")
+                    break;
+            }
+        }
+    }
+}
