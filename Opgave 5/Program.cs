72d3d559b34d0b401876174e8170778aa6185c40
emil41m5 <emil41m5@CV-BB-5340>
2021-08-18 14:46:15 +0200

    .

---
+++ Opgave 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Opgave_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Skriv exit for at stoppe.");
+                string input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
